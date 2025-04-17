fce6951fe1ef8ad2909562af501c6fb71a8dde09
Lukej-git <lukejtz@gmail.com>
2025-04-17 11:54:01 +0800

    Solving TFlint

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 # Comment out the below if you are working on local
 
 terraform {
+  required_version = ">= 1.3.0"
   backend "s3" {
     bucket = "sctp-ce9-tfstate"
     key    = "package-vul-scan-luke.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
